706eb2f39bf729a8b204e2e8cd0687190af6bfb3
CelineChole <github@celinechole.com>
2019-10-05 12:07:56 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let str = "beautiful day";
+
+// Do all exercises with both `for` and `while`
+
+// 1. Print all other letters
+
+// 2. Print index of 'e'
+
+// 3. Print the characters that occur at even indexes
+
+// 4. Print the characters that occur at odd indexes - do it backward
+
+// 5. Print with each character in this format "Index of (character) is (index)"
+
+// input: "hi"
+// output:
+    // "Index of h is 0"
+    // "Index of i is 1"
+
+
+let newSentence = "Today we are Saturday"
+
+// 1. Return a new string without the vowels
+
+ // input: "hello"
+ // output: "hll"
